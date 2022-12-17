ce05370102f05076f12b3009657ee63fb42ab386
madhan-8266 <65811223+Madhan-coder@users.noreply.github.com>
2022-12-17 14:29:32 +0530

    Update 3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from werkzeug.utils import secure_filename
 from flask import Flask,request,render_template
 
-UPLOAD_FOLDER = 'D:/Project/static/uploads'
+UPLOAD_FOLDER = 'D:/Project/python-sketch-converter/static/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
